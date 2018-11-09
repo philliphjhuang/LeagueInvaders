73cc703ad7e6509e9ce6ae236d5269e3a6d2f5fc
league <league@wts9.attlocal.net>
2018-11-08 16:57:38 -0800

    League Invaders Part 1

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,54 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	JFrame frame = new JFrame();
+	int width = 500; 
+	int height = 800;
+	
+	
+	
+	
+	
+public static void main(String[] args) {
+	
+	
+	
+}	
+
+	void setUp() {
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
