4232d4eb092bf874884c1491cb4b16671bb9ad2a
philliphjhuang <League@AdministorsiMac.attlocal.net>
2018-11-18 12:57:17 -0800

    0
    .

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,9 +20,32 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	
 	int currentState = MENU_STATE;
 	
-	void updateState() {
-	Part 6, step 4	
+	
+	void updateMenuState() {
 		
+	}
+	void updateGameState() {
+		
+	}
+	void updateEndState() {
+		
+	}
+	
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		
+		g.fillRect(0,0, WIDTH,HEIGHT);    
+		
+	}
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+
+		g.fillRect(0,0, 9999, 9999); 
+	}
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+
+		g.fillRect(0,0, 9999, 9999); 
 	}
 	
 	public GamePanel() {
@@ -53,7 +77,22 @@ public void actionPerformed(ActionEvent e) {
 repaint();
 GO.update();
 repaint();
-	
+
+if(currentState == MENU_STATE){
+
+    updateMenuState();
+
+}else if(currentState == GAME_STATE){
+
+    updateGameState();
+
+}else if(currentState == END_STATE){
+
+    updateEndState();
+
+}
+
+
 	
 	
 	
@@ -64,23 +103,48 @@ repaint();
 
 public void paintComponent(Graphics g){
 	GO.draw(g);
+	
+	if(currentState == MENU_STATE){
+
+	    drawMenuState(g);
+
+	}else if(currentState == GAME_STATE){
+
+	    drawGameState(g);
+
+	}else if(currentState == END_STATE){
+
+	    drawEndState(g);
+
+	}
+
         
 
 }
 @Override
 public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
-	System.out.println("aaa");
+
 }
 @Override
 public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
-	System.out.println("aaa");
+
+	if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+		System.out.println(WIDTH+" "+HEIGHT);
+		currentState ++;
+	}
+	
+	if(currentState > END_STATE){
+		
+        currentState = MENU_STATE;
+	}
+	
 }
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
-	System.out.println("aaa");
+
 }
 
 
